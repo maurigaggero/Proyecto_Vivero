78b6010233eaa9709482d4f5cf700c66c3dfadc2
Mauricio Gaggero <mauricio.gaggero@hotmail.com>
2021-02-19 18:24:40 -0300

    add authorization

--- Proyecto_Vivero/Server/Controllers/ClientesController.cs
+++ Proyecto_Vivero/Server/Controllers/ClientesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Proyecto_Vivero.Server.Data;
@@ -10,6 +11,7 @@ using System.Threading.Tasks;
 
 namespace Proyecto_Vivero.Server.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ClientesController : ControllerBase
--- Proyecto_Vivero/Server/Controllers/EstadisticasController.cs
+++ Proyecto_Vivero/Server/Controllers/EstadisticasController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Proyecto_Vivero.Server.Data;
 using Proyecto_Vivero.Shared;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace Proyecto_Vivero.Server.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class EstadisticasController : ControllerBase
