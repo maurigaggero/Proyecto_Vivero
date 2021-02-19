cf0dd6f0509e2654a58a13709e38db957f900955
Mauricio Gaggero <mauricio.gaggero@hotmail.com>
2021-02-19 18:35:15 -0300

    newtonsoft hijo de mil putaa

--- Proyecto_Vivero/Server/Startup.cs
+++ Proyecto_Vivero/Server/Startup.cs
@@ -26,6 +26,9 @@ namespace Proyecto_Vivero.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers().AddNewtonsoftJson
+                (options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
