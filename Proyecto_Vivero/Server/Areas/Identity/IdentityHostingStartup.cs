f0f9921d53d28e4fb08d65857fdabcfd75e894e7
Mauricio Gaggero <mauricio.gaggero@hotmail.com>
2021-02-23 16:49:47 -0300

    scaffold identity pages

---
+++ Proyecto_Vivero/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Proyecto_Vivero.Server.Data;
+using Proyecto_Vivero.Shared;
+
+[assembly: HostingStartup(typeof(Proyecto_Vivero.Server.Areas.Identity.IdentityHostingStartup))]
+namespace Proyecto_Vivero.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
